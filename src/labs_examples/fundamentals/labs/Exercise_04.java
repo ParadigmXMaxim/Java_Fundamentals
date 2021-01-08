2ea4b471546972c526e83e04efa749f84b6e3bed
ParadigmXMaxim <iiparadigm@yahoo.com>
2021-01-08 13:44:32 -0500

    Finished labs

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,22 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        byte bit = 1;
+        short countries = 194;
+        long ipv6 = 340_000_000_000_000_000l; //It is 340 Undecillion but I stopped short
+        float pi = 3.1415f;
+        double morePi = 3.14159265;
+        char middleInitial = 'R';
+        boolean answer = false;
+
+        System.out.println("Byte bit is: " + bit);
+        System.out.println("Short countries is: " + countries);
+        System.out.println("long ipv6 is: " + ipv6);
+        System.out.println("float pi is: " + pi);
+        System.out.println("Double morePi is: " + morePi);
+        System.out.println("Char middleInitial is: " + middleInitial);
+        System.out.println("Boolean answer is: " + answer);
+
 
     }
 
